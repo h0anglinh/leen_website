e0bc495c8d61977bde6fb18d25c2b2eaa4052e25
Hoang Linh <linh.hoang@outlook.cz>
2024-05-06 16:03:38 +0200

    new tariffs per group

--- pages/phones/tariffs/index.vue
+++ pages/phones/tariffs/index.vue
@@ -1,100 +1,83 @@
 <script setup lang="ts">
-const { data: tarrifs, error } = await useAsyncData("Tariffs", () =>
-  $fetch("/api/mobiles/tariff", { method: "get" })
-);
-
-const { data: group } = await useAsyncData("group", () =>
-  $fetch("/api/mobiles/group", { method: "GET" })
-);
-
-if (!group.value) {
-  throw createError({
-    statusCode: 400,
-    message: "try it again later",
-  });
-}
-const [defaultGroup] = group.value;
-
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import CryptoJS from "crypto-js";
+import type { Json, TablesInsert } from "~/typings/database.types";
+const { encryptKey, toet_pass, nepal_pass } = useRuntimeConfig().public;
+const headTitle = "My Docs";
 useHead({
-  title: "Tariffs",
+  title: headTitle,
+  meta: [{ property: "og:title", content: `${headTitle} | LH` }],
 });
 
-definePageMeta({
-  middleware: "auth",
-});
+// definePageMeta({
+//   middleware: "auth",
+// });
+
+const user = ref("");
+const router = useRouter();
+
+const users = ref([
+  {
+    user: "toet",
+    password: toet_pass,
+  },
+  {
+    user: "nepomuk",
+    password: nepal_pass,
+  },
+]);
+
+async function handleLogin() {
+  if (!user.value) {
+    alert("invalid user ");
+    return;
+  }
+
+  const theUser = user.value.toLowerCase().trim();
+  const deviceInfo = useDeviceInfo();
+
+  const passed = users.value.find((i) => i.user === theUser)?.user === theUser;
+  const mode = process.env.NODE_ENV;
+  const body: TablesInsert<"log"> = {
+    correct_pass: passed,
+    user: theUser,
+    meta: deviceInfo as any,
+    MODE: mode,
+  };
+
+  await $fetch("/api/logs", {
+    method: "POST",
+    body,
+  });
+
+  if (passed) {
+    const timestamp = new Date().getTime();
+    const code = CryptoJS.AES.encrypt(`${theUser}:${timestamp}`, encryptKey).toString();
+
+    router.push({ name: "phones-tariffs-group", params: { group: theUser }, query: { code } });
+  } else {
+    alert("Incorrect user / password");
+  }
+}
 </script>
 
 <template>
-  <v-container>
-    <h1 class="text-h1">Tariffs</h1>
-
-    <v-row v-if="tarrifs && tarrifs.length > 0">
-      <v-col v-for="(item, i) in tarrifs" :key="i" cols="12" md="6" lg="4">
+  <v-container class="h-screen">
+    <v-row justify="center" align="center" class="h-screen">
+      <v-col cols="12" sm="6" md="5">
         <v-card>
-          <v-card-title class="text-blue-darken-3">
-            <div class="d-flex justify-space-between">
-              <div>{{ item.name }}</div>
-              <div>
-                <v-chip class="ma-2" label color="blue" v-if="!item.action_price">
-                  {{
-                    (item.monthly_fee + item.monthly_fee * ((defaultGroup.margin || 0) / 100)).toFixed(0)
-                  }}
-                  CZK
-                </v-chip>
-                <v-chip class="ma-2" label color="red" v-else>
-                  {{
-                    (item.action_price + item.action_price * ((defaultGroup.margin || 0) / 100)).toFixed(
-                      0
-                    )
-                  }}
-                  CZK
-                </v-chip>
-              </div>
-            </div>
-          </v-card-title>
-          <v-card-text>
-            <v-table>
-              <thead>
-                <tr>
-                  <th class="text-left" colspan="2">Položka</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Data</td>
-                  <td class="text-right" v-if="item.data">
-                    <span v-if="item.data < 1000">{{ item.data }} MB</span>
-                    <span v-else-if="item.data >= 1000"> {{ item.data / 1000 }} GB </span>
-                  </td>
-                  <td v-else class="text-right">
-                    <v-chip color="success"> Neomezene </v-chip>
-                  </td>
-                </tr>
-                <template v-if="item.call_rate === item.sms_rate && item.call_rate === 0">
-                  <tr>
-                    <td>Volani a SMS</td>
-                    <td class="text-right"><v-chip color="success">Neomezene</v-chip></td>
-                  </tr>
-                </template>
-                <template v-else-if="item.call_rate === item.sms_rate">
-                  <tr>
-                    <td>Calls & SMS</td>
-                    <td class="text-right">{{ item.call_rate }} CZK</td>
-                  </tr>
-                </template>
-                <template v-else>
-                  <tr>
-                    <td>Calls</td>
-                    <td class="text-right">{{ item.call_rate }} CZK</td>
-                  </tr>
-                  <tr>
-                    <td>SMS</td>
-                    <td class="text-right">{{ item.sms_rate }} CZK</td>
-                  </tr>
-                </template>
-              </tbody>
-            </v-table>
-          </v-card-text>
+          <v-form @submit.prevent="handleLogin">
+            <v-card-title class="text-center"> Enter username </v-card-title>
+            <v-card-text>
+              <v-text-field v-model="user" label="User" type="text"></v-text-field>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="primary" type="submit">SUBMIT</v-btn>
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-form>
         </v-card>
       </v-col>
     </v-row>
